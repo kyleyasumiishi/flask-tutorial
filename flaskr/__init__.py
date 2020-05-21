dbfcb59cd811e18630cb3c0d2d7ed7e415ec6b04
Kyle <k.yasumiishi@gmail.com>
2020-05-20 22:14:01 -0700

    Created register view

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -23,9 +23,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     # a simple page that says hello    
     @app.route('/hello')
